1b87a3538d18f30acc89a5c6db549ebd46f75981
SeenSeens <truongtuan829@gmail.com>
2020-11-25 18:45:40 +0700

    dfd

--- wp-content/themes/cuisine/inc/woocommerce.php
+++ wp-content/themes/cuisine/inc/woocommerce.php
@@ -133,9 +133,21 @@ function change_existing_currency_symbol( $currency_symbol, $currency ) {
 	}
 	return $currency_symbol;
 }
-// xóa hành động 
 
-// function action_woocommerce_checkout_billing(  ) { 
-//     echo 'sasd'; 
-// }; 
-// add_action ( 'woocommerce_checkout_billing' , 'action_woocommerce_checkout_billing' , 10 , 0 ); 
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields',99 );
+function custom_override_checkout_fields( $fields ) {
+ 	//Danh sách field cần xóa ở đây
+ 	unset($fields['billing']['billing_first_name']);
+ 	unset($fields['billing']['billing_last_name']);
+ 	unset($fields['billing']['billing_company']);
+	unset($fields['billing']['billing_country']);
+	unset($fields['billing']['billing_address_1']);
+	unset($fields['billing']['billing_address_2']);
+	unset($fields['billing']['billing_postcode']);
+	unset($fields['billing']['billing_city']);
+	unset($fields['billing']['billing_phone']);
+	unset($fields['billing']['billing_email']);
+	unset($fields['order']['order_comments']);
+ 
+ return $fields;
+}
